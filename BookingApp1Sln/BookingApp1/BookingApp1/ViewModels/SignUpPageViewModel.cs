c3d415ebb4d1b6d91456f3395bd85785d1fed679
haafiz <haafizmowzer@gmail.com>
2019-12-06 12:49:48 -0800

    Edited

--- BookingApp1Sln/BookingApp1/BookingApp1/ViewModels/SignUpPageViewModel.cs
+++ BookingApp1Sln/BookingApp1/BookingApp1/ViewModels/SignUpPageViewModel.cs
@@ -1,15 +1,33 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace BookingApp1.ViewModels
 {
-    public class SignUpPageViewModel : BindableBase
+    public class SignUpPageViewModel : ViewModelBase
     {
-        public SignUpPageViewModel()
+
+        private DelegateCommand _navigateCommand;
+        public DelegateCommand NavigateCommand =>
+            _navigateCommand ?? (_navigateCommand = new DelegateCommand(ExecuteNavigateCommand));
+
+        public SignUpPageViewModel(INavigationService navigationService) : base(navigationService)
         {
+            Title = "Sign Up Page";
+
+        }
+
+         async void ExecuteNavigateCommand()
+        {
+            await NavigationService.NavigateAsync("MainPage");
+
+
+        
+        
+        
 
 
         }
